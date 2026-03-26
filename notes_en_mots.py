ef910a66c20ba3b8557c376febed3dc916f6dc7f
Daniel Bokingo Botay <bokingodaniel4@gmail.com.com>
2026-03-26 22:05:01 +0100

    ajout exercice: salutation

---
+++ notes_en_mots.py
@@ -0,0 +1,7 @@
+note= {
+    "Alice": 15, 
+    "Bob": 8, 
+    "Charlie": 12, 
+    "Diana": 5, 
+    "Eve": 10
+}
---
+++ salutation.py
@@ -0,0 +1,7 @@
+def saluer(nom, message= 'Bonjour'):
+    print(f"{message} {nom}!")
+
+saluer("Alice")
+saluer("Alice", "Salut")
+
+saluer("Alice", "Bonsoir")
